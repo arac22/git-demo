a25049ba045657440b1f8eef752d92a2a2112e52
arac22 <arac22@users.noreply.github.com>
2022-08-25 09:15:49 +0200

    nural network skeleton

--- sketch.js
+++ sketch.js
@@ -1,59 +1,7 @@
-let handpose;
-let video;
-let predictions = [];
-
 function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO);
-  video.size(width, height);
-
-  handpose = ml5.handpose(video, modelReady);
-
-  // This sets up an event that fills the global variable "predictions"
-  // with an array every time new hand poses are detected
-  handpose.on("predict", results => {
-    predictions = results;
-  });
-
-  // Hide the video element, and just show the canvas
-  video.hide();
-}
-
-function modelReady() {
-  console.log("Model ready!");
+  createCanvas(640, 480); 
 }
 
 function draw() {
   image(video, 0, 0, width, height);
-
-  // We can call both functions to draw all keypoints and the skeletons
-  drawKeypoints();
-  drawConfidence();
-}
-
-// A function to draw ellipses over the detected keypoints
-function drawKeypoints() {
-  for (let i = 0; i < predictions.length; i += 1) {
-    const prediction = predictions[i];
-    for (let j = 0; j < prediction.landmarks.length; j += 1) {
-      const keypoint = prediction.landmarks[j];
-      fill(0, 255, 0);
-      noStroke();
-      ellipse(keypoint[0], keypoint[1], 10, 10);
-    }
-	
-  }
-  if (predictions.length > 0){
-    console.log(predictions[0].handInViewConfidence);  
-  } else {
-    console.log(0);  
-  }
-}
-
-function drawConfidence(){
-  if (predictions.length > 0){
-    text(predictions[0].handInViewConfidence, 50, 50);  
-  } else {
-    text("Look mama no hands", 50, 50);  
-  }
 }
